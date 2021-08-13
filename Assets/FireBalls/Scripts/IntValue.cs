cd1cc2dec18bcb5d14a09050324ff625f06c7363
Mustafa Demir <mustafademir013@gmail.com>
2021-08-13 11:09:12 +0300

    add:score modules

---
+++ Assets/FireBalls/Scripts/IntValue.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "ScriptableObjects/IntValue", order = 2)]
+public class IntValue : ScriptableObject
+{
+    public int Score;
+}
