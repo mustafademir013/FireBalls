bbc899bc9f2dd7b5c1516cd6bac5e6df7b99f148
Mustafa Demir <mustafademir013@gmail.com>
2021-08-23 12:12:16 +0300

    add enviroment system

--- Assets/FireBalls/Scripts/Environment/EnviromentController.cs
+++ Assets/FireBalls/Scripts/Environment/EnviromentController.cs
@@ -1,9 +1,43 @@
+using System;
 using UnityEngine;
 
 public class EnviromentController : MonoBehaviour
 {
-    [SerializeField] private Transform _enviromentTr;
 
+    public static event Action PathSpawned; 
+
+
+    [SerializeField] private IntValue _levelNumber;
+    [SerializeField] private EnvironmentData _environmentData;
+    [SerializeField] private IntValue _gunData;
+
+    private void Start()
+    {
+        SpawnPath();
+        SpawnGun();
+        SpawnPlane();
+    }
+    public void SpawnPath()
+    {
+        string path = _environmentData.PlatformPath + _levelNumber.Value.ToString();
+        _environmentData.CurrentPlatform = Instantiate(Resources.Load<GameObject>(path)).transform;
+        _environmentData.CurrentPlatform.transform.parent = transform;
+        _environmentData.CurrentPlatform.transform.localPosition = Vector3.zero;
+        PathSpawned?.Invoke();
+    }
+    public void SpawnGun()
+    {
+        string path = _environmentData.GunPath + _gunData.Value.ToString();
+        _environmentData.CurrentGun = Instantiate(Resources.Load<GameObject>(path)).transform;
+        _environmentData.CurrentGun.transform.position = new Vector3(0,0,1);
+    }
+    public void SpawnPlane()
+    {
+        string path = _environmentData.PlanePath + _levelNumber.Value.ToString();
+        _environmentData.CurrentPlane = Instantiate(Resources.Load<GameObject>(path)).transform;
+        _environmentData.CurrentPlane.transform.parent = transform;
+        _environmentData.CurrentPlane.transform.localPosition = Vector3.zero;
+    }
     private void OnEnable()
     {
         CubeController.CubeFinished += Rotate;
@@ -13,7 +47,6 @@ public class EnviromentController : MonoBehaviour
     {
         CubeController.CubeFinished -= Rotate;
     }
-
     private void Rotate()
     {
         //float angle = circlePaths[_circleIndex].parent.rotation.y;
--- Assets/FireBalls/Scripts/Environment/EnvironmentData.cs
+++ Assets/FireBalls/Scripts/Environment/EnvironmentData.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObjects/Environment/EnvironmentData", order = 1)]
 public class EnvironmentData : ScriptableObject
 {
-    [SerializeField] private string _pathFolder;
-    public string PathFolder { get => _pathFolder;}
+    [SerializeField] private string _platformPath;
+    public string PlatformPath { get => _platformPath;}
 
-    private Transform _path;
-    public Transform Path { get => _path; set => _path = value; }
+    [SerializeField] private string _planePath;
+    public string PlanePath { get => _planePath; }
+
+    [SerializeField] private string _gunPath;
+    public string GunPath { get => _gunPath;}
+
+
+    private Transform _currentPlatformPath;
+    public Transform CurrentPlatform { get => _currentPlatformPath; set => _currentPlatformPath = value; }
+
+    private Transform _currentGun;
+    public Transform CurrentGun { get => _currentGun; set => _currentGun = value; }
+
+    private Transform _currentPlane;
+    public Transform CurrentPlane { get => _currentPlane; set => _currentPlane = value; }
 }
